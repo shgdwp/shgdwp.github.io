46d027091a9ed8611c0af226c622f95c51011780
shgdwp <87617850+shgdwp@users.noreply.github.com>
2021-07-18 18:38:05 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,4 +1,7 @@
-var firebaseConfig = {
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
     apiKey: "AIzaSyDwzalBpWeMXwIbilNC1SQC8qISNQqwFJo",
     authDomain: "shgdwp-afc23.firebaseapp.com",
     projectId: "shgdwp-afc23",
@@ -6,6 +9,4 @@ var firebaseConfig = {
     messagingSenderId: "1031178436316",
     appId: "1:1031178436316:web:f64325a49e6fe6965d7e19",
     measurementId: "G-CPMGY9316Y"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+});
